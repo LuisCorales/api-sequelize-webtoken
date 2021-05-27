8417da880bf332bdd74e08dfc2dd5fccff32e20d
Luis Corales <luisaugcorales@gmail.com>
2021-05-26 20:11:02 -0500

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const {verifyToken} = require('../middlewares/authorization');
+const jwt = require('jsonwebtoken')
 
+const authorizationRoutes = require('./authorization')
 const dashboardRoutes = require('./dashboard')
 
-router.use("/dashboard", dashboardRoutes);
+router.use("/auth", authorizationRoutes);
+router.use("/dashboard", verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            return res.sendStatus(403);
+        }
+
+        return res.json({
+            authData
+        });
+    });
+}, dashboardRoutes);
 
 module.exports = router;
