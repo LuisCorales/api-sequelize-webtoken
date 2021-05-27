7470d42df189a93dc0aa6f1320818b9db238e8ad
Luis Corales <luisaugcorales@gmail.com>
2021-05-26 23:27:22 -0500

    Update authorization.js

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,16 +1,29 @@
+const jwt = require('jsonwebtoken')
+
 /** Verify the token attached to the request */
 exports.verifyToken = (req, res, next) => {
-    // Get the auth header value
-    const bearerHeader = req.headers['authorization'];
+    try{
+        // Get the auth header value
+        const bearerHeader = req.headers['authorization'];
 
-    // Check if undefined
-    if(typeof bearerHeader === 'undefined') {
-        // Send 403 error: Forbidden
+        // Check if undefined
+        if(typeof bearerHeader === 'undefined') {
+            // Send 403 error: Forbidden
+            throw new error;
+        }
+
+        // Asign the token to the request
+        const bearer = bearerHeader.split(' ');
+        req.token = bearer[1];
+
+        jwt.verify(req.token, 'secretkey', (err) => {
+            if(err) {
+                throw err;
+            }
+        });
+
+        next();
+    } catch(e) {
         return res.sendStatus(403);
     }
-
-    // Asign the token to the request
-    const bearer = bearerHeader.split(' ');
-    req.token = bearer[1];
-    next();
 };
