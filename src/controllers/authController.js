e25ead5af5b49629eab59ccbddd2570248a3f1dc
Luis Corales <luisaugcorales@gmail.com>
2021-05-28 17:16:28 -0500

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,35 +10,27 @@ module.exports.post = async (req, res) => {
                 username: req.body.username
             }
         })
-
-        console.log('Test1')
     
         // If there are no users with the same username, throw error
         if(user.length < 1) {
             throw new Error;
         }
 
-        console.log('Test2')
-
         bcrypt.compare(req.body.password, user[0].password, (err, result) => {
             if (err) {
                 return res.sendStatus(401);
             }
 
-            console.log('Test3')
-
             // If they are the same, create token
             if (result) {
-                console.log('Test4')
                 jwt.sign({user}, 'secretkey', (err, token) => {
                     if(err) {
                         throw err;
                     }
-                    console.log('Test5')
             
                     return res.status(200).json({
                         token
-                    })
+                    });
                 });
             } else {
                 return res.sendStatus(401);
